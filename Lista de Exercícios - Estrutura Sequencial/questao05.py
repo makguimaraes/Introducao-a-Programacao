ef32d647a247f5c66031370dcc4a564232bc873b
makguimaraes <127980915+makguimaraes@users.noreply.github.com>
2023-05-19 07:16:33 -0300

    Questão 05
    Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius (C = 5 * ((F-32) / 9)):

---
+++ Lista de Exercícios - Estrutura Sequencial/questao05.py
@@ -0,0 +1,5 @@
+tfa = float(input('Digite o valor da temperatura em Fahrenheit que deseja converter para Celsius: '))
+
+tcelsius = 5 * ((tfa - 32) / 9)
+
+print(f'A temperatura de {tfa}°F corresponde a {tcelsius:.2f}°C.')
