b9e686eea6c7a96b75bf8cfb675d24e023af06a0
makguimaraes <127980915+makguimaraes@users.noreply.github.com>
2023-05-19 07:19:33 -0300

    Questão 07
    Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
    
    1. o produto do dobro do primeiro com metade do segundo .
    2. a soma do triplo do primeiro com o terceiro.
    3. o terceiro elevado ao cubo

---
+++ Lista de Exercícios - Estrutura Sequencial/questao07.py
@@ -0,0 +1,11 @@
+num1 = int(input('Digite um número inteiro: '))
+num2 = int(input('Digite outro número inteiro: '))
+num3 = float(input('Digite um número real: '))
+
+res1 = (2 * num1) * (num2 / 2)
+res2 = (3 * num1) + num3
+res3 = num3 ** 3
+
+print(f'O resultado da primeira operação é {res1}.')
+print(f'O resultado da segunda operação é {res2}.')
+print(f'O resultado da terceira operação é {res3}.')
