576863e7e6c10410a491bb3573183aaa56df8225
makguimaraes <127980915+makguimaraes@users.noreply.github.com>
2023-05-19 07:01:01 -0300

    Create questao01.py

---
+++ Lista de Exercícios - Estruturas de Decisão/questao01.py
@@ -0,0 +1,8 @@
+numero = float(input('Digite um número: '))
+
+if numero < 10:
+    print(f'{numero} é menor que 10.')
+elif numero > 10:
+    print(f'{numero} é maior que 10.')
+else:
+    print('Você digitou 10!')
