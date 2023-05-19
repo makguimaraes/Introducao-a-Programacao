5fa4a2a43c806ac6257b5ace2b365bfd2f8cffee
makguimaraes <127980915+makguimaraes@users.noreply.github.com>
2023-05-19 10:02:28 -0300

    Questão 01
    Use um dicionário para armazenar informações sobre uma pessoa que você conheça. Armazene seu primeiro nome, o sobrenome, a idade e a cidade em que ela vive. Você deverá ter chaves como primeiro_nome, sobrenome, idade, e cidade. Por fim, mostre cada informação armazenada em seu dicionário.

---
+++ Lista de Exercícios - Listas, Tuplas e Dicionários/questao01.py
@@ -0,0 +1,9 @@
+pessoa = {'primeiro_nome': 'Letícia',
+          'sobrenome': 'López',
+          'idade': '1',
+          'cidade': 'Santiago'}
+
+print('Primeiro nome:', pessoa['primeiro_nome'])
+print('Sobrenome:', pessoa['sobrenome'])
+print('Idade:', pessoa['idade'])
+print('Cidade:', pessoa['cidade'])
