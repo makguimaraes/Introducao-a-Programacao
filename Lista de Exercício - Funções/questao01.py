4323a79be203e09b87b3b574b09e43adcc783305
makguimaraes <127980915+makguimaraes@users.noreply.github.com>
2023-05-19 10:27:58 -0300

    Delete Lista de Exercício - Funções directory

--- Lista de Exercício - Funções/questao01.py
+++
@@ -1,9 +0,0 @@
-def somaImposto(taxaImposto, custo):
-    imposto = custo * (taxaImposto / 100)
-    custo += imposto
-    return custo
-
-taxaImposto = float(input('Digite a taxa sobre vendas: '))
-custo = float(input('Digite o custo do item:'))
-
-print(somaImposto(taxaImposto, custo))
--- Lista de Exercício - Funções/questao02.py
+++
@@ -1,9 +0,0 @@
-def contador_char(frase):
-    contador = len(frase)
-    return contador
-    
-frase = input('Digite a frase desejada: ')
-
-numero_char = contador_char(frase)
-
-print(f'A frase digitada tem {numero_char} caracteres.')
