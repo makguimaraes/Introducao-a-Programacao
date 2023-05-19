476aa3927db94421bba94a49cd650b6c8620f83e
makguimaraes <127980915+makguimaraes@users.noreply.github.com>
2023-05-19 07:02:08 -0300

    Create questao03.py

---
+++ Lista de Exercícios - Estruturas de Decisão/questao03.py
@@ -0,0 +1,6 @@
+qtd_macas = int(input('Quantas maçãs foram compradas? '))
+
+if qtd_macas >= 12:
+    print(f'A compra custou R${qtd_macas * 1.00}')
+else:
+    print(f'A compra custou R${qtd_macas * 1.30}')
