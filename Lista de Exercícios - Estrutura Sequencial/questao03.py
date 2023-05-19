0f757f6235ab586f18b2b322054fb3a088efbead
makguimaraes <127980915+makguimaraes@users.noreply.github.com>
2023-05-19 07:13:43 -0300

    Questão 03
    Faça um programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.

---
+++ Lista de Exercícios - Estrutura Sequencial/questao03.py
@@ -0,0 +1,5 @@
+lado_quadrado = float(input('Digite o lado do quadrado (em metros) cuja área deseja saber: '))
+
+area_quadrado = lado_quad**2
+
+print(f'O dobro da área do quadrado mede {2*areaquad} m².')
