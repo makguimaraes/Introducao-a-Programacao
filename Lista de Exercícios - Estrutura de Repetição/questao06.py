3e8d789b142e18ae7499342f6f762cbccf168589
makguimaraes <127980915+makguimaraes@users.noreply.github.com>
2023-05-19 06:59:50 -0300

    Create questao06.py

---
+++ Lista de Exercícios - Estrutura de Repetição/questao06.py
@@ -0,0 +1,44 @@
+convidados = ['Wolfgang Amadeus Mozart', 'Oscar Wilde', 'Jane Austen', 'Alan Turing', 'Ada Lovelace']
+qtd_convidados = len(convidados)
+
+def escolha():
+    opcao = input('O que deseja fazer?\n1 - Imprimir lista de convidados\n2 - Imprimir convites\n3 - Substituir convidado(s)\n4 - Sair\nDigite a opção desejada: ')
+    while True:
+        if opcao == '1':
+            impressao_lista()
+        elif opcao == '2':
+            impressao_convites()
+        elif opcao == '3':
+            substituicao_convidado()
+        elif opcao == '4':
+            break
+        else:
+            print('Digite uma opção válida!')
+
+def impressao_convites():
+    for i in range(0, qtd_convidados):
+        print(30*'-')
+        print('CONVITE')
+        print(30*'-')
+        print(f'\n{convidados[i]}, você está convidado para um jantar na minha casa, no dia XX/XX às XX:00h.\n')
+        print('ENDEREÇO: Rua das Avenidas, XXX\n\n')
+
+def impressao_lista():
+    print(30*'-')
+    print(f'Código do Convidado = {i}')
+    print(f'Nome do Convidado(a): {convidados[i]}')
+    print(30*'-')
+    print('\n')
+
+def substituicao_convidado():
+    convidado_faltoso = input('Digite o código do convidado faltoso: ')
+    convidados[convidado_faltoso] = input('Digite o nome do novo convidado: ')
+    outra_substituicao = input('Deseja substituir outro convidado? (S/N)')
+    if outra_substituicao != 's'or 'S':
+        escolha()
+
+print(30*'-')
+print('GERENCIADOR DE CONVITES')
+print(30*'-')
+
+escolha()
