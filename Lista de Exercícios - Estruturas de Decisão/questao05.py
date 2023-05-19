00ac94cc472e58832184ae26197b228b057554a0
makguimaraes <127980915+makguimaraes@users.noreply.github.com>
2023-05-19 07:04:12 -0300

    Create questao05.py

---
+++ Lista de Exercícios - Estruturas de Decisão/questao05.py
@@ -0,0 +1,9 @@
+numero1 = float(input('Digite o primeiro número: '))
+numero2 = float(input('Digite o segundo número: '))
+
+if numero1 > numero2:
+    print(f'{numero1} foi o maior número digitado.')
+elif numero2 > numero1:
+     print(f'{numero2} foi o maior número digitado.')
+else:
+     print('Os dois números digitados são iguais.')
