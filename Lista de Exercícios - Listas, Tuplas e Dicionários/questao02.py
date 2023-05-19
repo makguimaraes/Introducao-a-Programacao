0783303df501bbc4f9f57065366c7c08ecc1c95a
makguimaraes <127980915+makguimaraes@users.noreply.github.com>
2023-05-19 10:04:00 -0300

    Questão 02
    Use um dicionário para armazenar os números favoritos de algumas pessoas. Escolha cinco colegas, e pergunte quais seus números favoritos. Use seus nomes para serem as chaves de cada número favorito. Ao final, exiba o nome de cada pessoa e seu número favorito.

---
+++ Lista de Exercícios - Listas, Tuplas e Dicionários/questao02.py
@@ -0,0 +1,7 @@
+pessoas_numeros = {'pessoa1': 7,
+                   'pessoa2': 13,
+                   'pessoa3': 3,
+                   'pessoa4': 5,
+                   'pessoa5': 19}
+
+print(pessoas_numeros)
