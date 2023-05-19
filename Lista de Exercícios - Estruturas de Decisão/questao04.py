0dd26d95507613910e33d6e61f912b89ff344701
makguimaraes <127980915+makguimaraes@users.noreply.github.com>
2023-05-19 07:03:41 -0300

    Create questao04.py

---
+++ Lista de Exercícios - Estruturas de Decisão/questao04.py
@@ -0,0 +1,12 @@
+nome = input('Digite o nome do aluno: ')
+nota1 = float(input('Digite a primeira nota:'))
+nota2 = float(input('Digite a segunda nota: '))
+
+media = (nota1 + nota2) / 2
+
+print(f'A média de {nome} foi {media}')
+
+if media >= 6:
+    print(f'{nome} passou de ano!')
+else:
+    print(f'{nome} reprovou.')
