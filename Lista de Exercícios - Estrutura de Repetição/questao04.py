251197f7895ef98303d6147745469f675e79e1aa
makguimaraes <127980915+makguimaraes@users.noreply.github.com>
2023-05-19 07:37:24 -0300

    Questão 04
    Faça um programa que receba um número e que calcule e mostre a tabuada desse número:

--- Lista de Exercícios - Estrutura de Repetição/questao04.py
+++ Lista de Exercícios - Estrutura de Repetição/questao04.py
@@ -2,5 +2,5 @@ numero = int(input('Digite o número cuja tabuada deseja ver: '))
 
 for multiplicando in range(1,10):
     produto = numero * multiplicando
-    print(f'{numero} * {multiplicando} = {produto}')
+    print(f'{numero} * {multiplicando} = {produto} ')
     multiplicando += 1
