b5ced2e11cf8717b93e65001c43553e7a67de65a
makguimaraes <127980915+makguimaraes@users.noreply.github.com>
2023-05-19 07:11:09 -0300

    Questão 01
    Faça um programa que converta metros para centímetros:

---
+++ Lista de Exercícios - Estrutura Sequencial/questao01.py
@@ -0,0 +1,5 @@
+medidam = float(input('Digite o valor em metros que deseja converter para centímetros: '))
+
+medidacm = medidam * 100
+
+print(f'{medidam} metros corresponde a {medidacm} centímetros.')
