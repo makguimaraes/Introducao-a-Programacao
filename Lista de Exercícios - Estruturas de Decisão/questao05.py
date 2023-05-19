20c8018f61e69d5d334fb41239c9d6203d681309
makguimaraes <127980915+makguimaraes@users.noreply.github.com>
2023-05-19 07:44:20 -0300

    Questão 05
    Ler dois valores (considere que não serão lidos valores iguais) e escrever o maior deles:

--- Lista de Exercícios - Estruturas de Decisão/questao05.py
+++ Lista de Exercícios - Estruturas de Decisão/questao05.py
@@ -2,8 +2,8 @@ numero1 = float(input('Digite o primeiro número: '))
 numero2 = float(input('Digite o segundo número: '))
 
 if numero1 > numero2:
-    print(f'{numero1} foi o maior número digitado.')
+    print(f'{numero1} foi o maior número digitado. ')
 elif numero2 > numero1:
-     print(f'{numero2} foi o maior número digitado.')
+     print(f'{numero2} foi o maior número digitado. ')
 else:
-     print('Os dois números digitados são iguais.')
+     print('Os dois números digitados são iguais. ')
