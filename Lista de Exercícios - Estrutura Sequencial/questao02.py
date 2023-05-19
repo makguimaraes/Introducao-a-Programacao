b2268274a4725a11fd78a46d6ac640d95fee1c1e
makguimaraes <127980915+makguimaraes@users.noreply.github.com>
2023-05-19 07:12:16 -0300

    Questão 02
    Faça um programa que peça o raio de um círculo, calcule e mostre sua área:

---
+++ Lista de Exercícios - Estrutura Sequencial/questao02.py
@@ -0,0 +1,7 @@
+import math
+
+raio = float(input('Digite o raio do círculo (em cm) cuja área deseja saber: '))
+
+area = math.pi * raio**2
+
+print(f'A área do círculo supracitado é de {area:.2f}cm².')
